8567def022f5209307d3fda6871cb556e3c3f1b6
Dmytro <dimaha508@gmail.com>
2023-09-29 17:31:49 +0300

    First C# consoleApp

---
+++ lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Class1
+    {   
+        static void Main()
+        {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            if (name == "")
+                System.Console.WriteLine("Hello, world!");
+            else
+                Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
