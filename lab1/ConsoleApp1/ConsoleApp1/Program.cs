6f283a2b45d59b05577aaf5472cc12249c4c3a2c
Dmytro <dimaha508@gmail.com>
2023-10-04 22:10:45 +0300

    Simple int Read solution
    do-while loop

--- lab1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using static System.Math;    //Import Math from Framework Class Library
 namespace ConsoleApplication1
 {
     class Class1
-    {   
+    {
         static void Main()
         {
             string name;
@@ -13,6 +14,20 @@ namespace ConsoleApplication1
             else
                 Console.WriteLine("Hello, " + name + "!");
 
+            //Moving further to the Lab1 task1
+            Console.WriteLine("Moving further to the Lab1 task1...");
+
+            //But before we implement simple Read solution
+            do
+            {
+                int i = Console.Read();
+                if (i != -1)
+                    Console.WriteLine("{0} {1}", (char)i, i); //Shows up entered values and their codes
+                else
+                    break;
+            } while (true);
+
         }
     }
 }
+   
