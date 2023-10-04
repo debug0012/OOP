53a95de51cdb6f6aaf9e5be2744b5fdd3a5b4286
Dmytro <dimaha508@gmail.com>
2023-10-04 23:46:31 +0300

    Task1 Non-func + func
    51-56 non-func, 7-13 func

--- lab1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,13 @@ namespace ConsoleApplication1
 {
     class Class1
     {
+        static double CalculateTask1(double a, double b, double c)
+        {
+            // Using formula from my variant
+            double result = Sin(2 * a) / a - 3 + Atan(b) / c;
+
+            return result;
+        }
         static void Main()
         {
             string name;
@@ -19,14 +26,14 @@ namespace ConsoleApplication1
             //Console.WriteLine("But before please enter random int number or string:");
 
             //But before we implement simple Read solution
-           /* do
-            {
-                int i = Console.Read();
-                if (i != -1)                      
-                    Console.WriteLine("{0} {1}", (char)i, i); //Shows up entered values and their codes
-                else
-                    break;    
-            } while (true);*/
+            /* do
+             {
+                 int i = Console.Read();
+                 if (i != -1)                      
+                     Console.WriteLine("{0} {1}", (char)i, i); //Shows up entered values and their codes
+                 else
+                     break;    
+             } while (true);*/
 
             //Console.WriteLine("Then enter random text, enter END to stop this loop: ");
 
@@ -40,11 +47,18 @@ namespace ConsoleApplication1
                     break;
             } while (true) ;*/
 
-            double y = 0;
-            Console.WriteLine("Please enter a, b, and c values");
-            double a = Convert.ToDouble(Console.ReadLine());
-            double b = Convert.ToDouble(Console.ReadLine());
-            double c = Convert.ToDouble(Console.ReadLine());    
+
+            /* Console.WriteLine("Please enter a, b, and c DOUBLE values");
+            double aValue = Convert.ToDouble(Console.ReadLine());
+            double bValue = Convert.ToDouble(Console.ReadLine());                         //Non-function method
+            double cValue = Convert.ToDouble(Console.ReadLine());
+            double yResult = Sin(aValue + aValue) / aValue - 3 + Atan(bValue) / cValue;
+            Console.WriteLine("Result: {0}", yResult); */
+
+           double result = CalculateTask1(3.5, 5, 2);  //Using Formula Function
+
+
+            Console.WriteLine("Moving further to the Lab1 task2...");
         }
     }
 }
