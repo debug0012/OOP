af4ad5c3f40bc54c6aa5dd7f7de1b905b57e2da3
Dmytro <dimaha508@gmail.com>
2023-11-06 20:08:09 +0200

    Add lab3 class Exam; enum Education

---
+++ lab3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+enum Education {
+    Specialist,
+    Bachelor,
+    SecondEducation
+}
+class Exam {
+    string subjectName {  get; set; } //auto-realization property
+    int grade {  get; set; } //auto-realization property
+    DateTime examDate { get; set; }  //auto-realization property
+    
+    public Exam(string Subject, int Grade, DateTime ExamDate) //constructor with params
+    {
+        subjectName = Subject;
+        grade = Grade;
+        examDate = ExamDate;
+    }
+
+    public Exam()  //constructor without params and default values
+    {
+        subjectName = " ";
+        grade = 0;
+        examDate = DateTime.MinValue    }
+}
